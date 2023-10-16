3920074f9d57caf167b0127df406f5818f2a302a
daniilturpitka54 <daniil.turpitka@gmail.com>
2023-10-16 16:49:19 -0700

    message change

--- Main/src/App.java
+++ Main/src/App.java
@@ -31,7 +31,7 @@ public class App {
 
 
         if(profit >=0){
-            System.out.println("Based On Historical Average Market Performance you will earn $" + String.format("%,.2f", profit) +" If You Finance Your Vehicle Instead Of Buying It In Cash. Make Yhe Downpayment And Invest The Rest Of The Money In Index Fund Such As $SPY, $VOO, $SPYG, $VTI (Not A Financial Advice).");
+            System.out.println("Based On Historical Average Market Performance you will earn $" + String.format("%,.2f", profit) +" If You Finance Your Vehicle Instead Of Buying It In Cash. If This Amount Is Significant To You - Make Yhe Downpayment And Invest The Rest Of The Money In Index Fund Such As $SPY, $VOO, $SPYG, $VTI (Not A Financial Advice).");
         }
         else{
             System.out.println("Your Interest Rate Is Higher Than Historical Average Market Return So You Will loose $" + String.format("%,.2f", profit*(-1)) + " In Interest Payments If You Decide To Finance Your Vehicle. In This Situation Its Better To Buy It in Cash If You Can.");
